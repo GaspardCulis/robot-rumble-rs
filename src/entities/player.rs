1d06ceef05e7c1c3fdc0802768fe9dbe4aa00673
GaspardCulis <gasdev.lp@gmail.com>
2023-11-14 15:53:14 +0100

    Create PlayerBundle

--- src/entities/player.rs
+++ src/entities/player.rs
@@ -1,7 +1,21 @@
 use bevy::prelude::*;
 
+use crate::core::{
+    gravity::Mass,
+    physics::{Position, Velocity},
+};
+
 #[derive(Component)]
 pub struct Player;
+
+#[derive(Bundle)]
+struct PlayerBundle {
+    marker: Player,
+    position: Position,
+    velocity: Velocity,
+    mass: Mass,
+    sprite: SpriteSheetBundle,
+}
 
 pub fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
