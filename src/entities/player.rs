adf94ac1c09ea9f0384aa93bf849ec79a4668c66
GaspardCulis <gasdev.lp@gmail.com>
2023-11-20 14:37:57 +0100

    Added SpriteBundle to PlayerBundle

--- src/entities/player.rs
+++ src/entities/player.rs
@@ -10,12 +10,16 @@ const PLAYER_MASS: u32 = 800;
 #[derive(Component)]
 pub struct Player;
 
+#[derive(Resource)]
+pub struct PlayerSprite(Handle<Image>);
+
 #[derive(Bundle)]
 struct PlayerBundle {
     marker: Player,
     position: Position,
     velocity: Velocity,
     mass: Mass,
+    sprite_bundle: SpriteBundle,
 }
 
 impl Default for PlayerBundle {
@@ -25,16 +29,18 @@ impl Default for PlayerBundle {
             position: Position(Vec2::ZERO),
             velocity: Velocity(Vec2::ZERO),
             mass: Mass(PLAYER_MASS),
+            sprite_bundle: SpriteBundle {
+                ..Default::default()
+            },
         }
     }
 }
 
-pub fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
+pub fn spawn_player(mut commands: Commands, sprite: Res<PlayerSprite>) {
     commands.spawn((
         SpriteBundle {
-            texture: asset_server.load("img/player.png"),
+            texture: sprite.0.clone(),
             transform: Transform::from_scale(Vec3::splat(0.1)),
-
             ..default()
         },
         PlayerBundle {
@@ -48,5 +54,12 @@ pub fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup);
+    }
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let player_sprite = asset_server.load("img/player.png");
+    commands.insert_resource(PlayerSprite(player_sprite));
 }
