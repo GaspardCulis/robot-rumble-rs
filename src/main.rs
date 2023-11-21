b231db69a99c166c21d48fb7865388d6e40127ea
GaspardCulis <gasdev.lp@gmail.com>
2023-11-21 16:42:04 +0100

    Changed window resolution

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() {
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         title: "Game".into(),
-                        resolution: (640.0, 480.0).into(),
+                        resolution: (1280.0, 720.0).into(),
                         resizable: false,
                         ..default()
                     }),
