ecf6a58b314bc962c9fc59119aad3951a8020875
GaspardCulis <gasdev.lp@gmail.com>
2023-11-22 11:32:45 +0100

    Merge player::rotate_to_nearest_planet in player::player_physics

--- src/entities/player.rs
+++ src/entities/player.rs
@@ -11,7 +11,7 @@ use crate::{
     utils::math,
 };
 
-use super::planet::Radius;
+use super::planet::{Planet, Radius};
 
 const PLAYER_MASS: u32 = 800;
 const PLAYER_VELOCITY: f32 = 500.;
@@ -70,8 +70,7 @@ impl Plugin for PlayerPlugin {
             .add_state::<PlayerState>()
             .add_systems(Startup, spawn_player)
             .add_systems(Update, handle_keys)
-            .add_systems(Update, player_physics)
-            .add_systems(Update, rotate_to_nearest_planet);
+            .add_systems(Update, player_physics);
     }
 }
 
@@ -116,38 +115,33 @@ fn handle_keys(
 }
 
 fn player_physics(
-    mut query: Query<(&mut Position, &mut Rotation, &PlayerInputVelocity)>,
+    mut player_query: Query<(&mut Position, &mut Rotation, &PlayerInputVelocity), Without<Planet>>,
+    planet_query: Query<(&Position, &Radius), With<Planet>>,
+    mut player_state: Res<State<PlayerState>>,
     time: Res<Time>,
 ) {
-    for (mut position, mut rotation, velocity) in query.iter_mut() {
-        position.0 += velocity.0 * time.delta_seconds();
-        rotation.0 += time.delta_seconds();
-    }
-}
-
-fn rotate_to_nearest_planet(
-    mut player_query: Query<(&mut Rotation, &Position), With<Player>>,
-    planet_query: Query<(&Position, &Radius)>,
-    time: Res<Time>,
-) {
-    let (mut player_rotation, player_position) = player_query.single_mut();
+    let (mut player_position, mut player_rotation, velocity) = player_query.single_mut();
 
     // Find nearest planet
     let mut nearest_position = Vec2::ZERO;
+    let mut nearest_radius: u32 = 0;
     let mut nearest_distance = f32::MAX;
     for (position, radius) in planet_query.iter() {
         let distance = position.0.distance(player_position.0);
         if distance - (radius.0 as f32) < nearest_distance {
             nearest_position = position.0;
+            nearest_radius = radius.0;
             nearest_distance = distance;
         }
     }
 
+    // Rotate towards it
     let target_angle = (nearest_position.y - player_position.0.y)
         .atan2(nearest_position.x - player_position.0.x)
         + PI / 2.;
     let mut short_angle = (target_angle - player_rotation.0) % (2. * PI);
     short_angle = (2. * short_angle) % (2. * PI) - short_angle;
-
     player_rotation.0 += short_angle * time.delta_seconds() * 1.;
+
+    player_position.0 += velocity.0 * time.delta_seconds();
 }
