39ed270aec02e96a2b908441d369179c20e3ecf8
GaspardCulis <gasdev.lp@gmail.com>
2023-11-22 09:53:04 +0100

    Added core::physics::Rotation and player jumping

--- src/entities/player.rs
+++ src/entities/player.rs
@@ -1,10 +1,12 @@
+use std::f32::consts::PI;
+
 use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
 
 use crate::{
     core::{
         gravity::Mass,
-        physics::{Position, Velocity},
+        physics::{Position, Rotation, Velocity},
     },
     utils::math,
 };
@@ -29,6 +31,7 @@ struct PlayerBundle {
     marker: Player,
     position: Position,
     velocity: Velocity,
+    rotation: Rotation,
     input_velocity: PlayerInputVelocity,
     mass: Mass,
     sprite_bundle: SpriteBundle,
@@ -40,6 +43,7 @@ impl Default for PlayerBundle {
             marker: Player,
             position: Position(Vec2::ZERO),
             velocity: Velocity(Vec2::ZERO),
+            rotation: Rotation(0.),
             input_velocity: PlayerInputVelocity(Vec2::ZERO),
             mass: Mass(PLAYER_MASS),
             sprite_bundle: SpriteBundle {
@@ -74,30 +78,34 @@ impl Plugin for PlayerPlugin {
 }
 
 fn handle_keys(
-    mut query: Query<&mut PlayerInputVelocity>,
+    mut query: Query<(&mut PlayerInputVelocity, &mut Velocity, &Rotation)>,
     keyboard_input: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
-    let mut velocity = query.single_mut();
+    let (mut input_velocity, mut velocity, rotation) = query.single_mut();
     let delta = time.delta_seconds();
 
     if keyboard_input.any_just_pressed([KeyCode::Space, KeyCode::Z]) {
-        todo!("Jump");
+        velocity.0 += Vec2::from_angle(rotation.0).rotate(Vec2::Y) * PLAYER_VELOCITY;
     }
 
     if keyboard_input.pressed(KeyCode::D) {
-        velocity.0.x = math::lerp(velocity.0.x, PLAYER_VELOCITY, delta);
+        input_velocity.0.x = math::lerp(input_velocity.0.x, PLAYER_VELOCITY, delta);
     }
     if keyboard_input.pressed(KeyCode::Q) {
-        velocity.0.x = math::lerp(velocity.0.x, -PLAYER_VELOCITY, delta);
+        input_velocity.0.x = math::lerp(input_velocity.0.x, -PLAYER_VELOCITY, delta);
     }
     if keyboard_input.pressed(KeyCode::S) {
-        velocity.0.y = math::lerp(velocity.0.y, -PLAYER_VELOCITY * 0.75, delta);
+        input_velocity.0.y = math::lerp(input_velocity.0.y, -PLAYER_VELOCITY * 0.75, delta);
     }
 }
 
-fn player_physics(mut query: Query<(&mut Position, &PlayerInputVelocity)>, time: Res<Time>) {
-    for (mut position, velocity) in query.iter_mut() {
+fn player_physics(
+    mut query: Query<(&mut Position, &mut Rotation, &PlayerInputVelocity)>,
+    time: Res<Time>,
+) {
+    for (mut position, mut rotation, velocity) in query.iter_mut() {
         position.0 += velocity.0 * time.delta_seconds();
+        rotation.0 += time.delta_seconds();
     }
 }
