139d20c59374e0e402d7f460ac01990def3b303d
GaspardCulis <gasdev.lp@gmail.com>
2024-03-22 11:39:17 +0100

    camera: Changed clear color

--- src/core/camera.rs
+++ src/core/camera.rs
@@ -5,7 +5,13 @@ pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, |mut commands: Commands| {
-            commands.spawn(Camera2dBundle::default());
+            commands.spawn(Camera2dBundle {
+                camera: Camera {
+                    clear_color: ClearColorConfig::Custom(Color::hex("#171711").unwrap()),
+                    ..Default::default()
+                },
+                ..Default::default()
+            });
         });
     }
 }
