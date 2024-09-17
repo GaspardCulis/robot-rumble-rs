4d7a96c9bb430078466a319897bb7f30375d0f86
GaspardCulis <gaspard@gasdev.fr>
2024-09-17 09:01:09 +0200

    feat(planet::materials): Added `PlanetMaterialLayerInit` component And added relevant initialization systems

--- src/entities/planet/materials/mod.rs
+++ src/entities/planet/materials/mod.rs
@@ -1,4 +1,10 @@
-use bevy::{asset::load_internal_asset, prelude::*, sprite::Material2dPlugin};
+use std::marker::PhantomData;
+
+use bevy::{
+    asset::load_internal_asset,
+    prelude::*,
+    sprite::{Material2d, Material2dPlugin, MaterialMesh2dBundle},
+};
 
 mod clouds;
 mod common;
@@ -28,9 +34,56 @@ impl Plugin for PlanetMaterialsPlugin {
             Shader::from_wgsl
         );
 
-        app.add_plugins(Material2dPlugin::<CloudsMaterial>::default())
-            .add_plugins(Material2dPlugin::<CratersMaterial>::default())
-            .add_plugins(Material2dPlugin::<LandmassesMaterial>::default())
-            .add_plugins(Material2dPlugin::<UnderMaterial>::default());
+        app.add_plugins(PlanetMaterialPlugin::<CloudsMaterial>::default())
+            .add_plugins(PlanetMaterialPlugin::<CratersMaterial>::default())
+            .add_plugins(PlanetMaterialPlugin::<LandmassesMaterial>::default())
+            .add_plugins(PlanetMaterialPlugin::<UnderMaterial>::default());
+    }
+}
+
+#[derive(Default)]
+struct PlanetMaterialPlugin<M: Material2d>(PhantomData<M>);
+
+impl<M: Material2d> Plugin for PlanetMaterialPlugin<M>
+where
+    M::Data: PartialEq + Eq + core::hash::Hash + Clone,
+{
+    fn build(&self, app: &mut App) {
+        app.add_plugins(Material2dPlugin::<M>::default())
+            .add_systems(Update, instance_layer_material::<M>);
+    }
+}
+
+#[derive(Component)]
+pub struct PlanetMaterialLayerInit<M: Material2d> {
+    pub material: M,
+    pub scale: f32,
+    pub z_index: f32,
+}
+
+fn instance_layer_material<M: Material2d>(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut material: ResMut<Assets<M>>,
+    query: Query<(Entity, &PlanetMaterialLayerInit<M>)>,
+) {
+    for (entity, layer) in query.iter() {
+        let mesh_bundle_entity = commands
+            .spawn(MaterialMesh2dBundle {
+                mesh: meshes.add(Mesh::from(Rectangle::default())).into(),
+                transform: Transform::from_scale(Vec3::splat(layer.scale)).with_translation(Vec3 {
+                    x: 0.,
+                    y: 0.,
+                    z: layer.z_index,
+                }),
+                material: material.add(layer.material.clone()),
+                ..default()
+            })
+            .id();
+
+        commands
+            .entity(entity)
+            .push_children(&[mesh_bundle_entity])
+            .remove::<PlanetMaterialLayerInit<M>>();
     }
 }
