65fbabfa976dd7b0377cc2940932098ce6d2551a
GaspardCulis <gaspard@gasdev.fr>
2024-09-17 09:02:34 +0200

    refactor(planet): Refactor planet kind structure

--- src/entities/planet/mod.rs
+++ src/entities/planet/mod.rs
@@ -1,5 +1,5 @@
 use crate::core::{gravity::Mass, physics::Position};
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::prelude::*;
 use std::f64::consts::PI;
 
 mod kinds;
@@ -19,6 +19,7 @@ struct PlanetBundle {
     position: Position,
     radius: Radius,
     mass: Mass,
+    spatial: SpatialBundle,
 }
 
 impl Default for PlanetBundle {
@@ -28,6 +29,10 @@ impl Default for PlanetBundle {
             position: Position(Vec2::ZERO),
             radius: Radius(DEFAULT_RADIUS),
             mass: Mass(radius_to_mass(DEFAULT_RADIUS)),
+            spatial: SpatialBundle {
+                transform: Transform::from_scale(Vec3::splat((DEFAULT_RADIUS * 2) as f32)),
+                ..Default::default()
+            },
         }
     }
 }
@@ -41,54 +46,14 @@ impl Plugin for PlanetPlugin {
     }
 }
 
-fn spawn_planet(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut clouds_material: ResMut<Assets<materials::CloudsMaterial>>,
-    mut landmasses_material: ResMut<Assets<materials::LandmassesMaterial>>,
-    mut under_material: ResMut<Assets<materials::UnderMaterial>>,
-) {
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(Mesh::from(Rectangle::default())).into(),
-        transform: Transform::from_scale(Vec3::splat(DEFAULT_RADIUS as f32 * 2.0))
-            .with_translation(Vec3 {
-                x: 0.,
-                y: 0.,
-                z: -2.,
-            }),
-        material: under_material
-            .add(materials::UnderMaterial::default())
-            .clone(),
-        ..default()
-    });
+fn spawn_planet(mut commands: Commands) {
+    let kind = kinds::EarthLike::new();
 
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(Mesh::from(Rectangle::default())).into(),
-        transform: Transform::from_scale(Vec3::splat(DEFAULT_RADIUS as f32 * 2.0))
-            .with_translation(Vec3 {
-                x: 0.,
-                y: 0.,
-                z: -1.,
-            }),
-
-        material: landmasses_material
-            .add(materials::LandmassesMaterial::default())
-            .clone(),
-        ..default()
-    });
-
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(Mesh::from(Rectangle::default())).into(),
-        transform: Transform::from_scale(Vec3::splat(DEFAULT_RADIUS as f32 * 2.0)),
-        material: clouds_material
-            .add(materials::CloudsMaterial::default())
-            .clone(),
-        ..default()
-    });
-
-    commands.spawn(PlanetBundle {
-        ..Default::default()
-    });
+    commands
+        .spawn(PlanetBundle {
+            ..Default::default()
+        })
+        .insert(kind);
 }
 
 fn radius_to_mass(radius: u32) -> u32 {
