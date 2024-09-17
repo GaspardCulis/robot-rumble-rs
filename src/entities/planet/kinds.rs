b4a0297e6400d614294503deee7e073c781bc048
GaspardCulis <gaspard@gasdev.fr>
2024-09-17 10:04:38 +0200

    feat(planet::kinds): Added `MoonLike` planet kind

--- src/entities/planet/kinds.rs
+++ src/entities/planet/kinds.rs
@@ -30,3 +30,39 @@ impl EarthLike {
         }
     }
 }
+
+#[derive(Bundle)]
+pub struct MoonLike {
+    under: PlanetMaterialLayerInit<UnderMaterial>,
+    craters: PlanetMaterialLayerInit<CratersMaterial>,
+}
+
+impl MoonLike {
+    pub fn new() -> Self {
+        Self {
+            under: PlanetMaterialLayerInit {
+                material: UnderMaterial {
+                    common: CommonMaterial {
+                        size: 8.,
+                        light_origin: Vec2::new(0.25, 0.25),
+                        ..Default::default()
+                    },
+                    color1: Srgba::hex("#a3a7c2").unwrap().into(),
+                    color2: Srgba::hex("#4c6885").unwrap().into(),
+                    color3: Srgba::hex("#3a3f5e").unwrap().into(),
+                    light_border_1: 0.615,
+                    light_border_2: 0.729,
+                    ..Default::default()
+                },
+                scale: 1.,
+                z_index: -1.,
+            },
+
+            craters: PlanetMaterialLayerInit {
+                material: Default::default(),
+                scale: 1.,
+                z_index: 0.,
+            },
+        }
+    }
+}
