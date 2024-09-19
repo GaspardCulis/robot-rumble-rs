214c4355f018719149f0a1364f6cdf7b54219245
GaspardCulis <gaspard@gasdev.fr>
2024-09-19 17:52:26 +0200

    perf(main): Improve debug mode condition test

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,28 @@ mod entities;
 mod utils;
 
 fn main() {
-    App::new()
-        .add_plugins(
-            DefaultPlugins
-                .set(ImagePlugin::default_nearest())
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        title: "Game".into(),
-                        resolution: (1280.0, 720.0).into(),
-                        resizable: false,
-                        ..default()
-                    }),
+    let mut app = App::new();
+
+    app.add_plugins(
+        DefaultPlugins
+            .set(ImagePlugin::default_nearest())
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Game".into(),
+                    resolution: (1280.0, 720.0).into(),
+                    resizable: false,
                     ..default()
-                })
-                .build(),
-        )
-        .add_plugins(CorePlugins)
-        .add_plugins(EntitiesPlugins)
-        .add_plugins(WorldInspectorPlugin::new().run_if(|| cfg!(debug_assertions)))
-        .run();
+                }),
+                ..default()
+            })
+            .build(),
+    )
+    .add_plugins(CorePlugins)
+    .add_plugins(EntitiesPlugins);
+
+    if cfg!(debug_assertions) {
+        app.add_plugins(WorldInspectorPlugin::new());
+    }
+
+    app.run();
 }
