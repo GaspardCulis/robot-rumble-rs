5fadb7d476bc4a74c2f81b8a6eaf9b6b90389c6e
GaspardCulis <gaspard@gasdev.fr>
2024-10-02 20:31:19 +0200

    feat(worldgen): Added `GenerateWorldEvent` event

--- src/core/worldgen.rs
+++ src/core/worldgen.rs
@@ -1,8 +1,23 @@
 use bevy::prelude::*;
 
+use crate::entities::planet::SpawnPlanetEvent;
+
 pub struct WorldgenPlugin;
 impl Plugin for WorldgenPlugin {
     fn build(&self, app: &mut App) {
+        app.add_event::<GenerateWorldEvent>()
+            .add_systems(Update, handle_genworld_event);
+    }
+}
+
+#[derive(Event)]
+pub struct GenerateWorldEvent;
+
+fn handle_genworld_event(
+    mut events: EventReader<GenerateWorldEvent>,
+    mut planet_sapwn_events: EventWriter<SpawnPlanetEvent>,
+) {
+    for _ in events.read() {
         todo!()
     }
 }
