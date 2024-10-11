4616a5f4f3407e842574c5c8ccab42bf3dd0b2cb
GaspardCulis <gaspard@gasdev.fr>
2024-10-11 13:26:44 +0200

    fix(client): Fixed connection code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
-use client::{ClientCommands as _, IoConfig, NetConfig, NetcodeConfig};
+use lightyear::prelude::client::*;
 use lightyear::prelude::*;
-use network::shared_config;
+use network::{shared_config, PROTOCOL_ID};
 
 use core::CorePlugins;
 use entities::EntitiesPlugins;
@@ -13,20 +13,28 @@ mod entities;
 mod network;
 mod utils;
 
+const CLIENT_ADDR: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 4000);
+const SERVER_ADDR: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 5000);
+
 fn main() {
     // Network
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+    let auth = Authentication::Manual {
+        server_addr: SERVER_ADDR,
+        client_id: 0,
+        private_key: Key::default(),
+        protocol_id: PROTOCOL_ID,
+    };
 
     let netcode_config = NetcodeConfig::default();
 
-    let io_config = IoConfig::from_transport(client::ClientTransport::UdpSocket(addr));
+    let io_config = IoConfig::from_transport(client::ClientTransport::UdpSocket(CLIENT_ADDR));
 
     let client_config = client::ClientConfig {
         shared: shared_config(Mode::Separate),
         net: NetConfig::Netcode {
+            auth,
             config: netcode_config,
             io: io_config,
-            auth: Default::default(),
         },
         ..default()
     };
