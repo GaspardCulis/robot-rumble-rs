11b7e4ad3724479d72af20f7ca5c680fa22e154a
GaspardCulis <gaspard@gasdev.fr>
2024-10-11 15:06:01 +0200

    refactor(network): Moved app init logic to new `ClientNetworkPlugin` and `ServerNetworkPlugin`

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,17 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use lightyear::prelude::client::*;
-use lightyear::prelude::*;
-use network::{protocol::PROTOCOL_ID, shared_config};
+use network::ClientNetworkPlugin;
 
 use core::CorePlugins;
 use entities::EntitiesPlugins;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 mod core;
 mod entities;
 mod network;
 mod utils;
 
-const CLIENT_ADDR: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 4000);
-const SERVER_ADDR: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 5000);
-
 fn main() {
-    // Network
-    let auth = Authentication::Manual {
-        server_addr: SERVER_ADDR,
-        client_id: 0,
-        private_key: Key::default(),
-        protocol_id: PROTOCOL_ID,
-    };
-
-    let netcode_config = NetcodeConfig::default();
-
-    let io_config = IoConfig::from_transport(client::ClientTransport::UdpSocket(CLIENT_ADDR));
-
-    let client_config = client::ClientConfig {
-        shared: shared_config(Mode::Separate),
-        net: NetConfig::Netcode {
-            auth,
-            config: netcode_config,
-            io: io_config,
-        },
-        ..default()
-    };
-
-    let client_plugins = client::ClientPlugins::new(client_config);
-
-    // Build the app
     let mut app = App::new();
 
     app.add_plugins(
@@ -58,7 +28,7 @@ fn main() {
             })
             .build(),
     )
-    .add_plugins(client_plugins)
+    .add_plugins(ClientNetworkPlugin)
     .add_plugins(CorePlugins)
     .add_plugins(EntitiesPlugins)
     .add_systems(Startup, init);
--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,4 @@
 use std::collections::HashMap;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use std::time::Duration;
 
 use bevy::log::{Level, LogPlugin};
 use bevy::prelude::*;
@@ -8,15 +6,12 @@ use bevy::state::app::StatesPlugin;
 use entities::player::PlayerBundle;
 use lightyear::prelude::server::*;
 use lightyear::prelude::*;
-use network::protocol::PROTOCOL_ID;
-use network::shared_config;
+use network::ServerNetworkPlugin;
 
 mod core;
 mod entities;
 mod network;
 mod utils;
-
-const SERVER_ADDR: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 5000);
 
 #[derive(Resource, Default)]
 struct ClientsRecord(HashMap<ClientId, Entity>);
@@ -59,30 +54,6 @@ fn handle_disconnections(
 }
 
 fn main() {
-    let netcode_config = NetcodeConfig::default().with_protocol_id(PROTOCOL_ID);
-
-    let link_conditioner = LinkConditionerConfig {
-        incoming_latency: Duration::from_millis(100),
-        incoming_jitter: Duration::from_millis(0),
-        incoming_loss: 0.00,
-    };
-
-    let io_config = IoConfig::from_transport(server::ServerTransport::UdpSocket(SERVER_ADDR))
-        .with_conditioner(link_conditioner);
-
-    let net_config = NetConfig::Netcode {
-        config: netcode_config,
-        io: io_config,
-    };
-
-    let server_config = server::ServerConfig {
-        shared: shared_config(Mode::Separate),
-        net: vec![net_config],
-        ..Default::default()
-    };
-
-    let server_plugin = server::ServerPlugins::new(server_config);
-
     let mut app = App::new();
 
     app.add_plugins((MinimalPlugins, StatesPlugin))
@@ -91,7 +62,7 @@ fn main() {
             filter: "wgpu=error,bevy_render=info,bevy_ecs=warn".to_string(),
             ..default()
         })
-        .add_plugins(server_plugin)
+        .add_plugins(ServerNetworkPlugin)
         .init_resource::<ClientsRecord>()
         .add_systems(Startup, init)
         .add_systems(Update, (handle_connections, handle_disconnections))
