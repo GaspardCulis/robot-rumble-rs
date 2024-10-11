08ed818b1105a51b9814483ea7a8f9bff4ee1113
GaspardCulis <gaspard@gasdev.fr>
2024-10-11 11:20:45 +0200

    feat(server): Setup server config and launch simple app

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,45 @@
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::time::Duration;
+
+use bevy::prelude::*;
+use lightyear::connection::server::{IoConfig, NetConfig};
+use lightyear::prelude::*;
+use lightyear::server::config::NetcodeConfig;
+use network::{shared_config, PROTOCOL_ID};
+
 mod core;
 mod entities;
 mod network;
 mod utils;
 
 fn main() {
-    println!("YOOOO wasssup im server!");
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+
+    let netcode_config = NetcodeConfig::default().with_protocol_id(PROTOCOL_ID);
+
+    let link_conditioner = LinkConditionerConfig {
+        incoming_latency: Duration::from_millis(100),
+        incoming_jitter: Duration::from_millis(0),
+        incoming_loss: 0.00,
+    };
+
+    let io_config = IoConfig::from_transport(server::ServerTransport::UdpSocket(addr))
+        .with_conditioner(link_conditioner);
+
+    let net_config = NetConfig::Netcode {
+        config: netcode_config,
+        io: io_config,
+    };
+
+    let server_config = server::ServerConfig {
+        shared: shared_config(Mode::Separate),
+        net: vec![net_config],
+        ..Default::default()
+    };
+
+    let server_plugin = server::ServerPlugins::new(server_config);
+
+    let mut app = App::new();
+
+    app.add_plugins(server_plugin).run();
 }
