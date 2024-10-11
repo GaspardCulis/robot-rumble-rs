d8cfc21dd3d5f759680123162634473353cd46e2
GaspardCulis <gaspard@gasdev.fr>
2024-10-11 11:42:12 +0200

    feat(client): Added netcode config and `ClientPlugins`

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use client::{IoConfig, NetConfig, NetcodeConfig};
+use lightyear::prelude::*;
+use network::shared_config;
 
 use core::CorePlugins;
 use entities::EntitiesPlugins;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 mod core;
 mod entities;
+mod network;
 mod utils;
 
 fn main() {
+    // Network
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+
+    let netcode_config = NetcodeConfig::default();
+
+    let io_config = IoConfig::from_transport(client::ClientTransport::UdpSocket(addr));
+
+    let client_config = client::ClientConfig {
+        shared: shared_config(Mode::Separate),
+        net: NetConfig::Netcode {
+            config: netcode_config,
+            io: io_config,
+            auth: Default::default(),
+        },
+        ..default()
+    };
+
+    let client_plugins = client::ClientPlugins::new(client_config);
+
+    // Build the app
     let mut app = App::new();
 
     app.add_plugins(
@@ -25,6 +50,7 @@ fn main() {
             })
             .build(),
     )
+    .add_plugins(client_plugins)
     .add_plugins(CorePlugins)
     .add_plugins(EntitiesPlugins);
 
