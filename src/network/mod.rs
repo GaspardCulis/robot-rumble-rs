2d2348b20eab7011f3269c19ae76a432942e3093
GaspardCulis <gaspard@gasdev.fr>
2024-10-11 11:35:39 +0200

    Revert "refactor(network): Moved net config init code to new `network::net_config` fn"
    This reverts commit ccc076f68834494966bbea399aec7029f1e30913.

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,9 +1,6 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::time::Duration;
 
-use lightyear::connection::server::{IoConfig, NetConfig};
 use lightyear::prelude::*;
-use lightyear::server::config::NetcodeConfig;
 
 mod protocol;
 
@@ -16,25 +13,5 @@ pub fn shared_config(mode: Mode) -> SharedConfig {
             tick_duration: Duration::from_secs_f64(1.0 / 64.0),
         },
         mode,
-    }
-}
-
-pub fn net_config() -> NetConfig {
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 8080);
-
-    let netcode_config = NetcodeConfig::default().with_protocol_id(PROTOCOL_ID);
-
-    let link_conditioner = LinkConditionerConfig {
-        incoming_latency: Duration::from_millis(50),
-        incoming_jitter: Duration::from_millis(0),
-        incoming_loss: 0.00,
-    };
-
-    let io_config = IoConfig::from_transport(server::ServerTransport::UdpSocket(addr))
-        .with_conditioner(link_conditioner);
-
-    NetConfig::Netcode {
-        config: netcode_config,
-        io: io_config,
     }
 }
