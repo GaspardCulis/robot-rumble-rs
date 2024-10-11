97985e7daa7f92d55926112c6ef16138bbcd2afb
GaspardCulis <gaspard@gasdev.fr>
2024-10-11 11:20:18 +0200

    feat(network): Define protocol id and shared net config

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,1 +1,17 @@
+use std::time::Duration;
+
+use lightyear::prelude::*;
+
 mod protocol;
+
+pub const PROTOCOL_ID: u64 = 1;
+
+pub fn shared_config(mode: Mode) -> SharedConfig {
+    SharedConfig {
+        server_replication_send_interval: Duration::from_millis(40),
+        tick: TickConfig {
+            tick_duration: Duration::from_secs_f64(1.0 / 64.0),
+        },
+        mode,
+    }
+}
