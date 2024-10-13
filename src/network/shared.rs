4bfd8350e91a10b92d9e3847652daa30d9773d91
GaspardCulis <gaspard@gasdev.fr>
2024-10-13 16:26:12 +0200

    feat(network::shared): Translate `GenerateSystemMessage` to `GenerateWorldEvent`

--- src/network/shared.rs
+++ src/network/shared.rs
@@ -1,14 +1,17 @@
 use std::time::Duration;
 
 use bevy::prelude::*;
-use lightyear::prelude::*;
+use lightyear::{prelude::*, shared::events::components::MessageEvent};
+
+use crate::core::worldgen;
 
 use super::protocol;
 
 pub struct SharedNetworkPlugin;
 impl Plugin for SharedNetworkPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(protocol::ProtocolPlugin);
+        app.add_plugins(protocol::ProtocolPlugin)
+            .add_systems(Update, handle_generate_system_message);
     }
 }
 
@@ -19,5 +22,16 @@ pub fn shared_config(mode: Mode) -> SharedConfig {
             tick_duration: Duration::from_secs_f64(1.0 / 64.0),
         },
         mode,
+    }
+}
+
+pub fn handle_generate_system_message(
+    mut network_events: EventReader<MessageEvent<protocol::GenerateSystemMessage>>,
+    mut worldgen_events: EventWriter<worldgen::GenerateWorldEvent>,
+) {
+    for network_event in network_events.read() {
+        worldgen_events.send(worldgen::GenerateWorldEvent {
+            seed: network_event.message.seed,
+        });
     }
 }
