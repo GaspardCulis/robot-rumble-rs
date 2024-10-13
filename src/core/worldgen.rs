896539b802e549d528e1c740768ba3bc98b2e492
GaspardCulis <gaspard@gasdev.fr>
2024-10-13 19:17:59 +0200

    refactor(planet): Moved `PlanetLayerMaterialInit` spawn logic to `planet::config`

--- src/core/worldgen.rs
+++ src/core/worldgen.rs
@@ -77,7 +77,7 @@ fn handle_genworld_event(
                 let mut collision = false;
                 planets.iter().for_each(|planet| {
                     collision |= planet.position.0.distance(position.0)
-                        < (planet.radius.0 + radius.0) as f32 * 1.5
+                        < (planet.radius + radius).0 as f32 * 1.5
                 });
 
                 if !collision {
