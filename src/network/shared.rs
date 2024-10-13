d1889671c964b80d2ee815ff26cdf7f8d43723be
GaspardCulis <gaspard@gasdev.fr>
2024-10-13 17:06:34 +0200

    fix(network): Removed message based worldgen Cuz `EventReader<MessageEvent<T>>` seems to be currently broken

--- src/network/shared.rs
+++ src/network/shared.rs
@@ -1,17 +1,14 @@
 use std::time::Duration;
 
 use bevy::prelude::*;
-use lightyear::{prelude::*, shared::events::components::MessageEvent};
-
-use crate::core::worldgen;
+use lightyear::prelude::*;
 
 use super::protocol;
 
 pub struct SharedNetworkPlugin;
 impl Plugin for SharedNetworkPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(protocol::ProtocolPlugin)
-            .add_systems(Update, handle_generate_system_message);
+        app.add_plugins(protocol::ProtocolPlugin);
     }
 }
 
@@ -22,16 +19,5 @@ pub fn shared_config(mode: Mode) -> SharedConfig {
             tick_duration: Duration::from_secs_f64(1.0 / 64.0),
         },
         mode,
-    }
-}
-
-pub fn handle_generate_system_message(
-    mut network_events: EventReader<MessageEvent<protocol::GenerateSystemMessage>>,
-    mut worldgen_events: EventWriter<worldgen::GenerateWorldEvent>,
-) {
-    for network_event in network_events.read() {
-        worldgen_events.send(worldgen::GenerateWorldEvent {
-            seed: network_event.message.seed,
-        });
     }
 }
