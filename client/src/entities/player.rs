01c8052a1f0b727585056b894ff8cb17f223119f
GaspardCulis <gaspard@gasdev.fr>
2024-11-12 10:51:42 +0100

    refactor: Reworked code structure to multiple crates

---
+++ client/src/entities/player.rs
@@ -0,0 +1,120 @@
+use bevy::prelude::*;
+use bevy_asset_loader::prelude::*;
+use leafwing_input_manager::prelude::*;
+
+use lightyear::prelude::client::*;
+use lightyear::prelude::*;
+use lightyear::{
+    inputs::leafwing::input_buffer::InputBuffer, prelude::client::Predicted,
+    shared::replication::components::Controlled,
+};
+use robot_rumble_common::entities::player::*;
+
+use crate::core::camera::CameraFollowTarget;
+
+#[derive(AssetCollection, Resource)]
+struct PlayerAssets {
+    #[asset(path = "img/player.png")]
+    player: Handle<Image>,
+}
+
+pub struct ClientPlayerPlugin;
+impl Plugin for ClientPlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_collection::<PlayerAssets>()
+            .add_systems(Update, handle_new_player)
+            .add_systems(FixedUpdate, client_movement.after(player_physics));
+    }
+}
+
+fn handle_new_player(
+    mut commands: Commands,
+    player_query: Query<(Entity, Has<Controlled>), (Added<Predicted>, With<Player>)>,
+    sprite: Res<PlayerAssets>,
+) {
+    for (player_entity, is_controlled) in player_query.iter() {
+        let mut player_commands = commands.entity(player_entity);
+
+        if is_controlled {
+            info!("Own player replicated to us, adding inputmap to {player_entity:?}");
+            let input_map = InputMap::new([
+                // Jump
+                (PlayerAction::Jump, KeyCode::Space),
+                (PlayerAction::Jump, KeyCode::KeyW),
+                // Sneak
+                (PlayerAction::Sneak, KeyCode::ShiftLeft),
+                (PlayerAction::Sneak, KeyCode::KeyS),
+                // Directions
+                (PlayerAction::Right, KeyCode::KeyD),
+                (PlayerAction::Left, KeyCode::KeyA),
+            ]);
+
+            player_commands.insert((input_map, CameraFollowTarget));
+        } else {
+            info!("Remote player replicated to us: {player_entity:?}");
+        }
+
+        player_commands.insert((
+            Name::new(format!(
+                "{} Player",
+                if is_controlled {
+                    "Controlled"
+                } else {
+                    "Remote"
+                }
+            )),
+            SpriteBundle {
+                texture: sprite.player.clone(),
+                transform: Transform::from_scale(Vec3::splat(0.2)),
+                ..default()
+            },
+        ));
+    }
+}
+
+fn client_movement(
+    mut query: Query<
+        (
+            &ActionState<PlayerAction>,
+            &InputBuffer<PlayerAction>,
+            SharedApplyInputsQuery,
+        ),
+        (With<Player>, With<Predicted>),
+    >,
+    tick_manager: Res<TickManager>,
+    rollback: Option<Res<Rollback>>,
+) {
+    // max number of stale inputs to predict before default inputs used
+    const MAX_STALE_TICKS: u16 = 6;
+    // get the tick, even if during rollback
+    let tick = rollback
+        .as_ref()
+        .map(|rb| tick_manager.tick_or_rollback_tick(rb))
+        .unwrap_or(tick_manager.tick());
+
+    for (action_state, input_buffer, mut saiq) in query.iter_mut() {
+        // is the current ActionState for real?
+        if input_buffer.get(tick).is_some() {
+            // Got an exact input for this tick, staleness = 0, the happy path.
+            movement_behaviour(action_state, &mut saiq, tick);
+            continue;
+        }
+
+        // if the true input is missing, this will be leftover from a previous tick, or the default().
+        if let Some((prev_tick, prev_input)) = input_buffer.get_last_with_tick() {
+            let staleness = (tick - prev_tick).max(0) as u16;
+            if staleness > MAX_STALE_TICKS {
+                // input too stale, apply default input (ie, nothing pressed)
+                movement_behaviour(&ActionState::default(), &mut saiq, tick);
+            } else {
+                // apply a stale input within our acceptable threshold.
+                // we could use the staleness to decay movement forces as desired.
+                movement_behaviour(prev_input, &mut saiq, tick);
+            }
+        } else {
+            // no inputs in the buffer yet, can happen during initial connection.
+            // apply the default input (ie, nothing pressed)
+            movement_behaviour(action_state, &mut saiq, tick);
+        }
+    }
+}
---
+++ client/src/utils/mod.rs
@@ -0,0 +1,5 @@
+mod gradient;
+pub mod spritesheet;
+
+#[allow(unused_imports)]
+pub use gradient::gradient;
