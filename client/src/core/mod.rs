63c23a75f34db7d1200b9f85e8bc708a08959fb7
GaspardCulis <gaspard@gasdev.fr>
2025-01-16 14:02:47 +0100

    feat(client): Added stars background shader code

--- client/src/core/mod.rs
+++ client/src/core/mod.rs
@@ -1,10 +1,12 @@
 use bevy::prelude::{App, Plugin};
 
+pub mod background;
 pub mod camera;
 
 pub struct CorePlugins;
 impl Plugin for CorePlugins {
     fn build(&self, app: &mut App) {
-        app.add_plugins(camera::CameraPlugin);
+        app.add_plugins(background::BackgroundPlugin)
+            .add_plugins(camera::CameraPlugin);
     }
 }
