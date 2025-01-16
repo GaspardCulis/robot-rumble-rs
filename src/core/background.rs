ae7223f21639443c07c2815b6e35ceee7b6e0271
GaspardCulis <gaspard@gasdev.fr>
2025-01-16 14:02:47 +0100

    feat(client): Added stars background shader code

---
+++ src/core/background.rs
@@ -0,0 +1,85 @@
+use bevy::{
+    prelude::*,
+    render::render_resource::{AsBindGroup, ShaderRef},
+    sprite::{Material2d, Material2dPlugin, MaterialMesh2dBundle},
+};
+
+use crate::utils::gradient;
+
+#[derive(Component)]
+struct Background;
+
+pub struct BackgroundPlugin;
+impl Plugin for BackgroundPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(Material2dPlugin::<StarsMaterial>::default())
+            .add_systems(Startup, setup);
+    }
+}
+
+#[derive(Asset, Reflect, AsBindGroup, Debug, Clone)]
+struct StarsMaterial {
+    #[uniform(0)]
+    pub size: f32,
+    #[uniform(0)]
+    pub octaves: i32,
+    #[uniform(0)]
+    pub seed: f32,
+    #[uniform(0)]
+    pub pixels: f32,
+    // pub should_tile: bool,
+    // pub reduce_background: bool,
+    #[uniform(0)]
+    pub uv_correct: Vec2,
+    #[texture(1)]
+    #[sampler(2)]
+    colorscheme_texture: Option<Handle<Image>>,
+}
+
+impl Material2d for StarsMaterial {
+    fn fragment_shader() -> ShaderRef {
+        "shaders/background/stars.wgsl".into()
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StarsMaterial>>,
+    mut images: ResMut<Assets<Image>>,
+) {
+    let colorscheme = images.add(gradient(
+        &vec![0.0, 0.143, 0.286, 0.429, 0.571, 0.714, 0.857, 1.0],
+        &vec![
+            "#202215", "#3a2802", "#963c3c", "#ca5a2e", "#ff7831", "#f39949", "#ebc275", "#dfd785",
+        ]
+        .into_iter()
+        .map(|hex| Srgba::hex(hex).unwrap())
+        .collect(),
+    ));
+
+    let material = materials.add(StarsMaterial {
+        size: 10.0,
+        octaves: 8,
+        seed: 69.42,
+        pixels: 500.0,
+        // should_tile: false,
+        // reduce_background: false,
+        uv_correct: Vec2::ONE,
+        colorscheme_texture: Some(colorscheme),
+    });
+
+    commands.spawn((
+        Background,
+        MaterialMesh2dBundle {
+            mesh: meshes.add(Mesh::from(Rectangle::default())).into(),
+            transform: Transform::from_scale(Vec3::splat(500.0)).with_translation(Vec3 {
+                x: 0.,
+                y: 0.,
+                z: 0.,
+            }),
+            material,
+            ..default()
+        },
+    ));
+}
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::{App, Plugin};
 
+pub mod background;
 pub mod camera;
 pub mod gravity;
 pub mod physics;
@@ -8,7 +9,8 @@ pub mod worldgen;
 pub struct CorePlugins;
 impl Plugin for CorePlugins {
     fn build(&self, app: &mut App) {
-        app.add_plugins(camera::CameraPlugin)
+        app.add_plugins(background::BackgroundPlugin)
+            .add_plugins(camera::CameraPlugin)
             .add_plugins(gravity::GravityPlugin)
             .add_plugins(physics::PhysicsPlugin)
             .add_plugins(worldgen::WorldgenPlugin);
