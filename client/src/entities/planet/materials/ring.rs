75a3cc8c60da98952633865f9538dbfe2c77c2ee
GaspardCulis <gaspard@gasdev.fr>
2025-01-28 09:40:39 +0100

    fix(materials): Define `AlphaMode2d` to `Blend` on materials https://bevyengine.org/learn/migration-guides/0-14-to-0-15/#add-2d-opaque-phase-with-depth-buffer

--- client/src/entities/planet/materials/ring.rs
+++ client/src/entities/planet/materials/ring.rs
@@ -43,6 +43,10 @@ impl Material2d for RingMaterial {
     fn fragment_shader() -> ShaderRef {
         "shaders/planet/ring.wgsl".into()
     }
+
+    fn alpha_mode(&self) -> bevy::sprite::AlphaMode2d {
+        bevy::sprite::AlphaMode2d::Blend
+    }
 }
 
 impl PlanetMaterial for RingMaterial {
