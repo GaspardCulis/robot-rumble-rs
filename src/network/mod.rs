ca61d554f201cd3b57a8f5eabc2152911f71d435
GaspardCulis <gaspard@gasdev.fr>
2025-03-24 09:59:20 +0100

    feat(network): Handle player spawning and input replication

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,14 +1,24 @@
-use bevy::{prelude::*, utils::HashMap};
+use bevy::prelude::*;
+use bevy::utils::HashMap;
 use bevy_ggrs::*;
 use bevy_matchbox::prelude::*;
 use inputs::GgrsSessionInput as _;
-use leafwing_input_manager::prelude::ActionState;
+use leafwing_input_manager::prelude::{ActionState, InputMap};
 
-use crate::entities::player::PlayerAction;
+use crate::{
+    core::{
+        camera,
+        physics::{PhysicsSet, Position},
+    },
+    entities::player::{Player, PlayerAction, PlayerBundle, PlayerSkin},
+};
 
-mod inputs;
+pub mod inputs;
 
-type SessionConfig = bevy_ggrs::GgrsConfig<u8, PeerId>;
+pub type SessionConfig = bevy_ggrs::GgrsConfig<u8, PeerId>;
+
+#[derive(Component, Reflect)]
+pub struct LocalPlayer;
 
 pub struct NetworkPlugin;
 impl Plugin for NetworkPlugin {
@@ -16,7 +26,8 @@ impl Plugin for NetworkPlugin {
         app.add_plugins(GgrsPlugin::<SessionConfig>::default())
             .add_systems(Startup, start_matchbox_socket)
             .add_systems(Update, wait_for_players)
-            .add_systems(ReadInputs, read_local_inputs);
+            .add_systems(ReadInputs, read_local_inputs)
+            .add_systems(GgrsSchedule, update_remote_inputs.before(PhysicsSet));
     }
 }
 
@@ -61,21 +72,75 @@ fn wait_for_players(mut commands: Commands, mut socket: ResMut<MatchboxSocket>) 
         .expect("failed to start session");
 
     commands.insert_resource(bevy_ggrs::Session::P2P(ggrs_session));
+
+    // Spawn players
+    let system_id = commands.register_system(spawn_players);
+    commands.run_system(system_id);
+}
+
+fn spawn_players(mut commands: Commands, session: Res<bevy_ggrs::Session<SessionConfig>>) {
+    let p2p_session = match &*session {
+        Session::P2P(p2_psession) => p2_psession,
+        _ => unimplemented!(),
+    };
+
+    assert_eq!(p2p_session.local_player_handles().len(), 1);
+    for local_handle in p2p_session.local_player_handles() {
+        // Local player
+        let input_map = InputMap::new([
+            // Jump
+            (PlayerAction::Jump, KeyCode::Space),
+            (PlayerAction::Jump, KeyCode::KeyW),
+            // Sneak
+            (PlayerAction::Sneak, KeyCode::ShiftLeft),
+            (PlayerAction::Sneak, KeyCode::KeyS),
+            // Directions
+            (PlayerAction::Right, KeyCode::KeyD),
+            (PlayerAction::Left, KeyCode::KeyA),
+        ]);
+
+        commands.spawn((
+            input_map,
+            LocalPlayer,
+            PlayerBundle::new(local_handle, Position(Vec2::ZERO)),
+            PlayerSkin("laika".into()),
+            camera::CameraFollowTarget,
+        ));
+    }
+
+    // Remote players
+    for remote_handle in p2p_session.remote_player_handles() {
+        commands.spawn((
+            PlayerBundle::new(remote_handle, Position(Vec2::ZERO)),
+            PlayerSkin("laika".into()),
+        ));
+    }
 }
 
 fn read_local_inputs(
     mut commands: Commands,
-    query: Query<&ActionState<PlayerAction>>,
+    query: Query<(&Player, &ActionState<PlayerAction>), With<LocalPlayer>>,
     local_players: Res<LocalPlayers>,
 ) {
     let mut local_inputs = HashMap::new();
 
-    for handle in &local_players.0 {
-        let action_state = query.single();
+    assert_eq!(local_players.0.len(), query.iter().len());
+    for (player, action_state) in query.iter() {
+        let handle = player.handle;
         let input = action_state.as_ggrs_session_input();
 
-        local_inputs.insert(*handle, input);
+        local_inputs.insert(handle, input);
     }
 
     commands.insert_resource(LocalInputs::<SessionConfig>(local_inputs));
+}
+
+fn update_remote_inputs(
+    mut query: Query<(&Player, &mut ActionState<PlayerAction>), Without<LocalPlayer>>,
+    inputs: Res<PlayerInputs<SessionConfig>>,
+) {
+    for (player, mut action_state) in query.iter_mut() {
+        let (input, _) = inputs[player.handle];
+        *action_state = ActionState::<PlayerAction>::from_ggrs_session_input(input);
+    }
 }
