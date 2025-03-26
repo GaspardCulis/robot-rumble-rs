850975b0a3b7c6a2a1021dad34cba7c2c87db4ca
GaspardCulis <gaspard@gasdev.fr>
2025-03-26 18:32:51 +0100

    fix(network): Rework state transitions No more sync issues (almost)

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -28,6 +28,9 @@ pub type SessionConfig = bevy_ggrs::GgrsConfig<u8, PeerId>;
 #[derive(Resource, Default, Clone, Copy, Debug, Deref, DerefMut)]
 struct SessionSeed(u64);
 
+#[derive(Resource, Default)]
+struct StartMatchDelay(Timer);
+
 pub struct NetworkPlugin;
 impl Plugin for NetworkPlugin {
     fn build(&self, app: &mut App) {
@@ -48,28 +51,19 @@ impl Plugin for NetworkPlugin {
             )
             .add_systems(
                 OnEnter(GameState::WorldGen),
-                (
-                    generate_world,
-                    prespawn_players.run_if(p2p_mode),
-                    spawn_synctest_players.run_if(synctest_mode),
-                ),
+                (generate_world, spawn_synctest_players.run_if(synctest_mode)),
             )
             .add_systems(
                 OnEnter(GameState::InGame),
-                (
-                    position_players,
-                    add_local_player_components.run_if(p2p_mode),
-                )
-                    .chain(),
-            )
-            .add_systems(
-                GgrsSchedule,
-                tick_worldgen_cooldown.run_if(in_state(GameState::WorldGen)),
+                (spawn_players, add_local_player_components)
+                    .chain()
+                    .run_if(p2p_mode),
             )
             .add_systems(
                 Update,
                 (
                     wait_for_players.run_if(in_state(GameState::MatchMaking).and(p2p_mode)),
+                    wait_start_match.run_if(in_state(GameState::WorldGen).and(p2p_mode)),
                     handle_ggrs_events.run_if(in_state(GameState::InGame)),
                 ),
             );
@@ -109,6 +103,25 @@ fn wait_for_players(
         seed ^= peer_id.0 ^ peer_id.1;
     }
     commands.insert_resource(SessionSeed(seed));
+    commands.insert_resource(StartMatchDelay(Timer::from_seconds(0.5, TimerMode::Once)));
+
+    next_state.set(GameState::WorldGen);
+}
+
+fn wait_start_match(
+    mut commands: Commands,
+    mut socket: ResMut<MatchboxSocket>,
+    mut next_state: ResMut<NextState<GameState>>,
+    mut timeout: ResMut<StartMatchDelay>,
+    time: Res<Time>,
+) {
+    timeout.0.tick(time.delta());
+    if !timeout.0.finished() {
+        return;
+    }
+
+    let players = socket.players();
+    assert_eq!(players.len(), NUM_PLAYERS);
 
     let mut session_builder = ggrs::SessionBuilder::<SessionConfig>::new()
         .with_num_players(NUM_PLAYERS)
@@ -131,21 +144,40 @@ fn wait_for_players(
 
     commands.insert_resource(bevy_ggrs::Session::P2P(ggrs_session));
 
-    next_state.set(GameState::WorldGen);
+    next_state.set(GameState::InGame);
 }
 
-/// Prespawn all players regardless if they are local or not. Positioning will be done later
-fn prespawn_players(mut commands: Commands, session: Res<bevy_ggrs::Session<SessionConfig>>) {
+fn spawn_players(
+    mut commands: Commands,
+    planets_query: Query<(&physics::Position, &Radius), With<Planet>>,
+    session: Res<bevy_ggrs::Session<SessionConfig>>,
+    seed: Res<SessionSeed>,
+) {
+    let mut rng = Xoshiro256PlusPlus::seed_from_u64(seed.0);
     let num_players = match &*session {
         Session::SyncTest(s) => s.num_players(),
         Session::P2P(s) => s.num_players(),
         Session::Spectator(s) => s.num_players(),
     };
 
+    let mut planets = planets_query.iter().collect::<Vec<_>>();
+    planets.sort_by(|(a_pos, _), (b_pos, _)| {
+        a_pos.length_squared().total_cmp(&b_pos.length_squared())
+    });
+    planets.shuffle(&mut rng);
+
     for handle in 0..num_players {
+        let (spawn_planet_pos, spawn_planet_radius) = planets
+            .get(handle)
+            .expect("Should have more planets than players");
+
+        let random_direction = Vec2::from_angle(rng.random::<f32>() * 2. * std::f32::consts::PI);
+        let position =
+            spawn_planet_pos.0 + random_direction * (spawn_planet_radius.0 as f32 + PLAYER_RADIUS);
+
         commands
             .spawn((
-                PlayerBundle::new(handle, physics::Position(Vec2::ZERO)),
+                PlayerBundle::new(handle, physics::Position(position)),
                 PlayerSkin("laika".into()),
             ))
             .add_rollback();
@@ -159,41 +191,16 @@ fn generate_world(
     worldgen_events.send(worldgen::GenerateWorldEvent { seed: seed.0 });
 }
 
-fn position_players(
-    mut players_query: Query<
-        (&Player, &mut physics::Position, &mut physics::Velocity),
-        (With<Player>, Without<Planet>),
-    >,
-    planets_query: Query<(&physics::Position, &Radius), With<Planet>>,
-    seed: Res<SessionSeed>,
-) {
-    let mut rng = Xoshiro256PlusPlus::seed_from_u64(seed.0);
-
-    let mut planets = planets_query.iter().collect::<Vec<_>>();
-    planets.sort_by(|(a_pos, _), (b_pos, _)| {
-        a_pos.length_squared().total_cmp(&b_pos.length_squared())
-    });
-    planets.shuffle(&mut rng);
-
-    for (player, mut player_position, mut player_velocity) in players_query.iter_mut() {
-        player_velocity.0 = Vec2::ZERO;
-
-        let (spawn_planet_pos, spawn_planet_radius) = planets
-            .get(player.handle)
-            .expect("Should have more planets than players");
-
-        let random_direction = Vec2::from_angle(rng.random::<f32>() * 2. * std::f32::consts::PI);
-        player_position.0 =
-            spawn_planet_pos.0 + random_direction * (spawn_planet_radius.0 as f32 + PLAYER_RADIUS);
-        player_position.0 = Vec2::ZERO + random_direction * 300.0;
-    }
-}
-
 fn add_local_player_components(
     mut commands: Commands,
     query: Query<(Entity, &Player)>,
-    local_players: Res<LocalPlayers>,
+    session: Res<bevy_ggrs::Session<SessionConfig>>,
 ) {
+    let local_players = match &*session {
+        Session::P2P(p2_psession) => p2_psession.local_player_handles(),
+        _ => unimplemented!(),
+    };
+
     let input_map = InputMap::new([
         // Jump
         (PlayerAction::Jump, KeyCode::Space),
@@ -208,7 +215,7 @@ fn add_local_player_components(
 
     let local_players_query = query
         .iter()
-        .filter(|(_, player)| local_players.0.contains(&player.handle))
+        .filter(|(_, player)| local_players.contains(&player.handle))
         .collect::<Vec<_>>();
 
     assert_eq!(local_players_query.len(), 1);
@@ -217,13 +224,4 @@ fn add_local_player_components(
     commands
         .entity(player_entity)
         .insert((input_map, CameraFollowTarget));
-}
-
-fn tick_worldgen_cooldown(
-    frame: Res<RollbackFrameCount>,
-    mut next_state: ResMut<NextState<GameState>>,
-) {
-    if frame.0 > 5 {
-        next_state.set(GameState::InGame);
-    }
 }
