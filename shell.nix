ed8a189e20f65f13717f35703165464698fea36e
GaspardCulis <gaspard@gasdev.fr>
2025-04-01 09:24:23 +0200

    chore(shell): Add `wasm-bindgen-cli` package

--- shell.nix
+++ shell.nix
@@ -1,27 +1,32 @@
-{ pkgs ? import <nixpkgs> { } }:
-
+{pkgs ? import <nixpkgs> {}}:
 with pkgs;
+  mkShell rec {
+    nativeBuildInputs = [
+      pkg-config
+      rustc
+      cargo
+      rustfmt
+      rust-analyzer
+      clippy
+      wasm-bindgen-cli
+    ];
+    buildInputs = [
+      clang
+      llvmPackages.bintools
+      udev
+      alsa-lib
+      vulkan-loader
+      xorg.libX11
+      xorg.libXcursor
+      xorg.libXi
+      xorg.libXrandr # To use the x11 feature
+      libxkbcommon
+      wayland # To use the wayland feature
+    ];
+    LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 
-mkShell rec {
-  nativeBuildInputs = [
-    pkg-config
-    rustc
-    cargo
-    rustfmt
-    rust-analyzer
-    clippy
-  ];
-  buildInputs = [
-    clang
-    llvmPackages.bintools
-    udev alsa-lib vulkan-loader
-    xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
-    libxkbcommon wayland # To use the wayland feature
-  ];
-  LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-
-  shellHook = ''
-    export PATH="$HOME/.cargo/bin:$PATH"
-  '';
-}
+    shellHook = ''
+      export PATH="$HOME/.cargo/bin:$PATH"
+    '';
+  }
