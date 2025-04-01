cbf7c360f945751e6a41496246b830a36a4df7bf
GaspardCulis <gaspard@gasdev.fr>
2025-04-01 08:42:27 +0200

    fix(camera): Fix player sprite rendering artifacts

--- src/core/camera.rs
+++ src/core/camera.rs
@@ -10,7 +10,7 @@ pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, |mut commands: Commands| {
-            commands.spawn(Camera2d);
+            commands.spawn((Camera2d, Msaa::Off));
         })
         .add_systems(GgrsSchedule, camera_movement.after(PhysicsSet::Movement));
     }
