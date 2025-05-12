194f55e736c4e983f6cda696012dd9b72cabc32b
Matthieu Rua <matthieu.rua@etu.univ-grenoble-alpes.fr>
2025-05-12 14:58:51 +0200

    feat(worldgen): Chnaged the world generation by using a ron configuration file

--- src/core/worldgen.rs
+++ src/core/worldgen.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_common_assets::ron::RonAssetPlugin;
 use rand::Rng;
 use rand_xoshiro::{rand_core::SeedableRng as _, Xoshiro256PlusPlus};
 use serde::{Deserialize, Serialize};
@@ -10,9 +11,16 @@ use super::physics::Position;
 pub struct WorldgenPlugin;
 impl Plugin for WorldgenPlugin {
     fn build(&self, app: &mut App) {
-        app.add_event::<GenerateWorldEvent>()
+        app.add_plugins(RonAssetPlugin::<WorldgenConfig>::new(&[]))
+            .add_event::<GenerateWorldEvent>()
+            .add_systems(Startup, load_worldgen_config)
             .add_systems(Update, handle_genworld_event);
     }
+}
+
+fn load_worldgen_config(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let config: Handle<WorldgenConfig> = asset_server.load("config/worldgen.ron");
+    commands.insert_resource(WorldgenConfigHandle(config));
 }
 
 #[derive(serde::Deserialize, Asset, TypePath)]
@@ -29,19 +37,8 @@ pub struct WorldgenConfig {
     min_planet_surface_distance: u32,
 }
 
-// TODO: Actual ron config file
-const WORLDGEN_CONFIG: WorldgenConfig = WorldgenConfig {
-    central_star_radius: 100,
-
-    min_planets: 8,
-    max_planets: 10,
-
-    min_planet_radius: 80,
-    max_planet_radius: 400,
-
-    max_planet_surface_distance: 2000,
-    min_planet_surface_distance: 100,
-};
+#[derive(Resource)]
+struct WorldgenConfigHandle(pub Handle<WorldgenConfig>);
 
 #[derive(Component, Debug, Reflect, Copy, Clone, PartialEq, Serialize, Deserialize)]
 pub struct GenerationSeed(pub u64);
@@ -54,30 +51,36 @@ pub struct GenerateWorldEvent {
 fn handle_genworld_event(
     mut events: EventReader<GenerateWorldEvent>,
     mut planet_spawn_events: EventWriter<SpawnPlanetEvent>,
+    config_handle: Res<WorldgenConfigHandle>,
+    configs: Res<Assets<WorldgenConfig>>,
 ) {
+    let Some(worldgen_config) = configs.get(&config_handle.0) else {
+        warn!("Worldgen config not loaded yet");
+        return;
+    };
+
     for GenerateWorldEvent { seed } in events.read() {
         let mut rng = Xoshiro256PlusPlus::seed_from_u64(*seed);
         let mut planets = Vec::new();
         planets.push(SpawnPlanetEvent {
             position: Position(Vec2::ZERO),
-            radius: Radius(WORLDGEN_CONFIG.central_star_radius),
+            radius: Radius(worldgen_config.central_star_radius),
             r#type: PlanetType::Star,
             seed: rng.random(),
         });
 
         let num_planets: u32 =
-            rng.random_range(WORLDGEN_CONFIG.min_planets..WORLDGEN_CONFIG.max_planets);
+            rng.random_range(worldgen_config.min_planets..worldgen_config.max_planets);
         for i in 0..num_planets {
             info!("Generating planet [{}/{}]", i + 1, num_planets);
 
             loop {
                 let random_direction = Vec2::from_angle(rng.random_range(1..360) as f32);
                 let radius = Radius(rng.random_range(
-                    WORLDGEN_CONFIG.min_planet_radius..WORLDGEN_CONFIG.max_planet_radius,
+                    worldgen_config.min_planet_radius..worldgen_config.max_planet_radius,
                 ));
                 let distance = rng.random_range(
-                    WORLDGEN_CONFIG.min_planet_surface_distance
-                        ..WORLDGEN_CONFIG.max_planet_surface_distance,
+                    worldgen_config.min_planet_surface_distance..worldgen_config.max_planet_surface_distance,
                 );
                 let position = Position(random_direction * distance as f32);
 
