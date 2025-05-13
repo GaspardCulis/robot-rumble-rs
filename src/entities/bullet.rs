dc625645955c73b0da269f7cc724d63a737a8b25
GaspardCulis <gaspard@gasdev.fr>
2025-05-13 16:23:26 +0200

    feat(entities): Add bullet entity

---
+++ src/entities/bullet.rs
@@ -0,0 +1,52 @@
+use bevy::prelude::*;
+
+use crate::core::{
+    gravity::{Mass, Passive},
+    physics::Position,
+};
+
+use super::planet::Radius;
+
+pub const BULLET_SPEED: f32 = 1200.;
+pub const BULLET_MASS: u32 = 20;
+
+#[derive(Component, Reflect)]
+#[require(Visibility)]
+pub struct Bullet;
+
+pub struct BulletPlugin;
+impl Plugin for BulletPlugin {
+    fn build(&self, app: &mut App) {
+        app.register_required_components_with::<Bullet, Mass>(|| Mass(BULLET_MASS))
+            .register_required_components_with::<Bullet, Passive>(|| Passive)
+            .register_required_components_with::<Bullet, Name>(|| Name::new("Bullet"))
+            .add_systems(Update, (add_sprite, check_collisions));
+    }
+}
+
+fn add_sprite(
+    mut commands: Commands,
+    query: Query<Entity, (Added<Bullet>, Without<Sprite>)>,
+    asset_server: Res<AssetServer>,
+) {
+    for bullet in query.iter() {
+        commands
+            .entity(bullet)
+            .insert(Sprite::from_image(asset_server.load("bullet.png")));
+    }
+}
+
+fn check_collisions(
+    mut commands: Commands,
+    bullet_query: Query<(Entity, &Position), With<Bullet>>,
+    planet_query: Query<(&Position, &Radius)>,
+) {
+    for (bullet, bullet_position) in bullet_query.iter() {
+        for (planet_position, planet_radius) in planet_query.iter() {
+            let distance = bullet_position.distance(planet_position.0) - planet_radius.0 as f32;
+            if distance <= 0.0 {
+                commands.entity(bullet).despawn();
+            }
+        }
+    }
+}
