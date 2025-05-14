aa3360a92c526c91990aba376cde111a4d5119a9
GaspardCulis <gaspard@gasdev.fr>
2025-05-14 14:48:29 +0200

    fix(materials): Add missing wasm padding to star material layers

--- src/entities/planet/materials/star.rs
+++ src/entities/planet/materials/star.rs
@@ -14,6 +14,8 @@ pub struct StarMaterial {
     pub common: super::CommonMaterial,
     #[uniform(1)]
     pub tiles: f32,
+    #[uniform(1)]
+    _wasm_padding: Vec3,
     #[texture(2)]
     #[sampler(3)]
     colorscheme_texture: Option<Handle<Image>>,
@@ -65,6 +67,7 @@ impl PlanetMaterial for StarMaterial {
             common,
             tiles: config.tiles,
             colorscheme_texture: Some(images.add(gradient)),
+            _wasm_padding: Default::default(),
         }
     }
 }
