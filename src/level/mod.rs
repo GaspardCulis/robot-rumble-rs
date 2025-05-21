04ee9cb1bcf9845d06f9845dd496c5055beffc9e
GaspardCulis <gaspard@gasdev.fr>
2025-05-21 11:15:13 +0200

    refactor(level): Rename `map_limit` mod to `limit`

--- src/level/mod.rs
+++ src/level/mod.rs
@@ -1,12 +1,12 @@
 use bevy::prelude::{App, Plugin};
 
-mod map_limit;
+mod limit;
 mod spawn;
 
 pub struct LevelPlugins;
 impl Plugin for LevelPlugins {
     fn build(&self, app: &mut App) {
-        app.add_plugins(map_limit::MapLimitPlugin)
+        app.add_plugins(limit::MapLimitPlugin)
             .add_plugins(spawn::MapSpawnPlugin);
     }
 }
