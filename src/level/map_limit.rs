169253b6fa4e338eb7c0c516f6542254e218bfb7
GaspardCulis <gaspard@gasdev.fr>
2025-05-21 10:19:31 +0200

    feat(map_limit): Add `DeathEvent`

--- src/level/map_limit.rs
+++ src/level/map_limit.rs
@@ -7,17 +7,21 @@ use crate::{
 };
 
 #[derive(Resource, Reflect)]
-pub struct MapLimit {
+struct MapLimit {
     // TODO: Reconsider using existing Radius component
     radius: f32,
     /// Precomputed for performance optimizations
     radius_squared: f32,
 }
 
+#[derive(Event)]
+pub struct DeathEvent(pub Entity);
+
 pub struct MapLimitPlugin;
 impl Plugin for MapLimitPlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<MapLimit>()
+            .add_event::<DeathEvent>()
             .insert_resource(MapLimit {
                 radius: 3000.0,
                 radius_squared: 3000.0f32.powi(2),
@@ -28,13 +32,14 @@ impl Plugin for MapLimitPlugin {
 
 fn check_outsiders(
     mut commands: Commands,
+    mut death_events: EventWriter<DeathEvent>,
     query: Query<(Entity, &Position, Has<Player>)>,
     limit: Res<MapLimit>,
 ) {
     for (entity, position, is_player) in query.iter() {
         if position.length_squared() > limit.radius_squared {
             if is_player {
-                todo!()
+                death_events.send(DeathEvent(entity));
             } else {
                 commands.entity(entity).despawn_recursive();
             }
