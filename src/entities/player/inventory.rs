801f53cc666837a9829e2cd3bb5d9e021b864fba
GaspardCulis <gaspard@gasdev.fr>
2025-05-21 15:18:11 +0200

    feat(inventory): Create `InventoryPlugin` that requires `Arsenal` for `Player`

--- src/entities/player/inventory.rs
+++ src/entities/player/inventory.rs
@@ -1,6 +1,24 @@
 use bevy::prelude::*;
 
-use super::weapons;
+use super::{Player, weapons};
 
 #[derive(Component, Debug, Reflect)]
 pub struct Arsenal(pub Vec<weapons::WeaponType>);
+
+pub struct InventoryPlugin;
+impl Plugin for InventoryPlugin {
+    fn build(&self, app: &mut App) {
+        app.register_type::<Arsenal>()
+            .register_required_components::<Player, Arsenal>();
+    }
+}
+
+impl Default for Arsenal {
+    fn default() -> Self {
+        Self(vec![
+            weapons::WeaponType::Pistol,
+            weapons::WeaponType::Shotgun,
+            weapons::WeaponType::Rifle,
+        ])
+    }
+}
