a2956f99fd84811ff12850849804a97a85e4fafd
GaspardCulis <gaspard@gasdev.fr>
2025-06-02 16:35:15 +0200

    feat: Create `misc::discord_presence` mod

---
+++ src/misc/discord_presence.rs
@@ -0,0 +1,26 @@
+use bevy::prelude::*;
+use discord_presence::{Client, Event};
+
+const APPLICATION_ID: u64 = 1379103471302737931;
+
+#[derive(Resource)]
+struct DiscordPresence(Client);
+
+pub struct DiscordPresencePlugin;
+impl Plugin for DiscordPresencePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup);
+    }
+}
+
+fn setup(mut commands: Commands) {
+    let mut drpc = Client::new(APPLICATION_ID);
+    drpc.on_ready(|_ctx| {
+        info!("Discord presence ready");
+    })
+    .persist();
+
+    drpc.start();
+
+    commands.insert_resource(DiscordPresence(drpc));
+}
