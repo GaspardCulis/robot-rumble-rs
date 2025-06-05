665134390f21161d21b235ae03e05335f2eb0fa2
GaspardCulis <gaspard@gasdev.fr>
2025-06-05 21:33:30 +0200

    refactor(planet): Now using `AssetCollection` for sprites and config

--- src/assets.rs
+++ src/assets.rs
@@ -2,19 +2,23 @@ use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
 use bevy_common_assets::ron::RonAssetPlugin;
 
-use crate::entities::satellite::assets as satellite_assets;
+use crate::entities::{planet, satellite::assets as satellite};
 
 /// Responsible for loading all crate's assets, and registering their asset loaders.
 pub struct AssetsPlugin;
 impl Plugin for AssetsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(RonAssetPlugin::<satellite_assets::SatelliteConfig>::new(&[
-            "ron",
+        app.add_plugins(RonAssetPlugin::<satellite::SatelliteConfig>::new(&[
+            "satellites.ron",
+        ]))
+        .add_plugins(RonAssetPlugin::<planet::PlanetsConfig>::new(&[
+            "planets.ron",
         ]))
         .add_loading_state(
             LoadingState::new(crate::GameState::AssetLoading)
                 .continue_to_state(crate::GameState::MatchMaking)
-                .load_collection::<satellite_assets::SatelliteAssets>(),
+                .load_collection::<planet::PlanetAssets>()
+                .load_collection::<satellite::SatelliteAssets>(),
         );
     }
 }
