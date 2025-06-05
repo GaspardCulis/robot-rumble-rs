9c5ba399c2b7e2e4ca55aa9b284e624fb6d8a1ee
GaspardCulis <gaspard@gasdev.fr>
2025-06-05 10:12:50 +0200

    docs(networl::config): Add documentation comments to config fields

--- src/network/config.rs
+++ src/network/config.rs
@@ -3,14 +3,20 @@ use bevy::prelude::*;
 #[serde_with::serde_as]
 #[derive(Asset, TypePath, serde::Deserialize)]
 pub struct NetworkConfig {
+    /// Amount of frames GGRS will delay the inputs for local players.
     pub input_delay: usize,
+    // TODO: Figure out what it does
     pub session_fps: usize,
     #[allow(dead_code)] // Follow https://github.com/gschup/bevy_ggrs/issues/121
+    /// Changes the rate at which GgrsSchedule is called per frame.
     pub schedule_fps: usize,
+    // TODO: Figure out what it does
     pub check_distance: usize,
     pub max_prediction_window: usize,
     #[serde_as(as = "serde_with::DurationSecondsWithFrac")]
+    /// Sets the disconnect timeout. The session will automatically disconnect from a remote peer if it has not received a packet in the timeout window.
     pub disconnect_timeout: std::time::Duration,
+    /// Sets the desync detection mode. With desync detection, the session will compare checksums for all peers to detect discrepancies / desyncs between peers.
     pub desync_detection: DesyncDetectionConfig,
 }
 
