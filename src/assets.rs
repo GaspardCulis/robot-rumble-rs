283ae82035a6516d894173bf67409ef82ab0ac1d
GaspardCulis <gaspard@gasdev.fr>
2025-06-05 21:43:42 +0200

    refactor(worldgen): Now using `AssetCollection` for sprites and config

--- src/assets.rs
+++ src/assets.rs
@@ -2,21 +2,28 @@ use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
 use bevy_common_assets::ron::RonAssetPlugin;
 
-use crate::entities::{planet, satellite::assets as satellite};
+use crate::{
+    core::worldgen,
+    entities::{planet, satellite::assets as satellite},
+};
 
 /// Responsible for loading all crate's assets, and registering their asset loaders.
 pub struct AssetsPlugin;
 impl Plugin for AssetsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(RonAssetPlugin::<satellite::SatelliteConfig>::new(&[
-            "satellites.ron",
+        app.add_plugins(RonAssetPlugin::<worldgen::WorldgenConfig>::new(&[
+            "worldgen.ron",
         ]))
         .add_plugins(RonAssetPlugin::<planet::PlanetsConfig>::new(&[
             "planets.ron",
         ]))
+        .add_plugins(RonAssetPlugin::<satellite::SatelliteConfig>::new(&[
+            "satellites.ron",
+        ]))
         .add_loading_state(
             LoadingState::new(crate::GameState::AssetLoading)
                 .continue_to_state(crate::GameState::MatchMaking)
+                .load_collection::<worldgen::WorldgenAssets>()
                 .load_collection::<planet::PlanetAssets>()
                 .load_collection::<satellite::SatelliteAssets>(),
         );
