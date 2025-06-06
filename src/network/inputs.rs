9cc0259687e5f808d61aa2cf6d6d5f8245de42d2
GaspardCulis <gaspard@gasdev.fr>
2025-06-06 16:35:42 +0200

    Revert "perf(network::inputs): Send pointer_direction only when shooting"
    This reverts commit cd8089c72b18b827621837398385a860dd79b76c.

--- src/network/inputs.rs
+++ src/network/inputs.rs
@@ -1,6 +1,6 @@
 use crate::{
     core::physics::PhysicsSet,
-    entities::player::{Player, PlayerAction, Weapon, weapon},
+    entities::player::{Player, PlayerAction},
     network::SessionConfig,
 };
 use bevy::{platform::collections::HashMap, prelude::*};
@@ -43,28 +43,19 @@ impl Plugin for NetworkInputsPlugin {
 
 fn read_local_inputs(
     mut commands: Commands,
-    query: Query<(&Player, &ActionState<PlayerAction>, &Weapon)>,
-    weapon_query: Query<&weapon::WeaponState>,
+    query: Query<(&Player, &ActionState<PlayerAction>)>,
     local_players: Res<LocalPlayers>,
 ) {
     let mut local_inputs = HashMap::new();
 
-    for (player, action_state, weapon) in query.iter() {
+    for (player, action_state) in query.iter() {
         let handle = player.handle;
 
         if !local_players.0.contains(&handle) {
             continue;
         }
 
-        let mut input = action_state.as_ggrs_session_input();
-
-        // Avoids rollbacks for other peers as pointer_direction cannot be predicted
-        if let Some(weapon_state) = weapon_query.get(weapon.0).ok() {
-            if !(weapon_state.can_fire() && action_state.pressed(&PlayerAction::Shoot)) {
-                input.keys = input.keys & !INPUT_SHOOT;
-                input.pointer_direction = Vec2::ZERO;
-            }
-        }
+        let input = action_state.as_ggrs_session_input();
 
         local_inputs.insert(handle, input);
     }
@@ -112,7 +103,12 @@ impl GgrsSessionInput for ActionState<PlayerAction> {
 
         NetworkInputs {
             keys,
-            pointer_direction: self.axis_pair(&PlayerAction::PointerDirection),
+            // Avoids rollbacks for other peers as pointer_direction cannot be predicted
+            pointer_direction: if keys & INPUT_SHOOT != 0 {
+                self.axis_pair(&PlayerAction::PointerDirection)
+            } else {
+                Vec2::ZERO
+            },
         }
     }
 
