4fcfd2c0e3ae67dc10219e8a9e18536e82bd1d62
Maxim Frolov <max.fro2021@gmail.com>
2025-06-18 14:10:32 +0200

    chore(core): aplhabetic order for plugins

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -11,12 +11,12 @@ pub mod worldgen;
 pub struct CorePlugins;
 impl Plugin for CorePlugins {
     fn build(&self, app: &mut App) {
-        app.add_plugins(background::BackgroundPlugin)
+        app.add_plugins(audio::AudioPlugin)
+            .add_plugins(background::BackgroundPlugin)
             .add_plugins(camera::CameraPlugin)
             .add_plugins(gravity::GravityPlugin)
             .add_plugins(inputs::InputsPlugin)
             .add_plugins(physics::PhysicsPlugin)
-            .add_plugins(worldgen::WorldgenPlugin)
-            .add_plugins(audio::AudioPlugin);
+            .add_plugins(worldgen::WorldgenPlugin);
     }
 }
