24831e0feb67b0cf936b27d6000c56fc79b250b8
GaspardCulis <gaspard@gasdev.fr>
2025-07-02 22:08:27 +0200

    chore(audio): Remove dead code

--- src/core/audio.rs
+++ src/core/audio.rs
@@ -1,13 +1,5 @@
 use bevy::prelude::*;
-use bevy::prelude::{App, Plugin};
-use bevy_kira_audio::AudioSource;
 use bevy_kira_audio::prelude::*;
-
-#[allow(dead_code)] // Temporarly until sound effects are passed by events
-#[derive(Event)]
-pub struct SoundEvent {
-    pub handle: Handle<AudioSource>,
-}
 
 pub struct GameAudioPlugin;
 
@@ -17,15 +9,6 @@ pub struct AudioSFX;
 
 impl Plugin for GameAudioPlugin {
     fn build(&self, app: &mut App) {
-        app.add_event::<SoundEvent>()
-            .add_plugins(AudioPlugin)
-            .add_audio_channel::<AudioSFX>();
-    }
-}
-
-#[allow(dead_code)] // Temporarly until sound is completely managed by audio module
-fn _play_sound(mut events: EventReader<SoundEvent>, sfx_channel: Res<AudioChannel<AudioSFX>>) {
-    for SoundEvent { handle } in events.read() {
-        sfx_channel.play(handle.clone());
+        app.add_plugins(AudioPlugin).add_audio_channel::<AudioSFX>();
     }
 }
